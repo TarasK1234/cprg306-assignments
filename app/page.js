fce3ef0bf2391fed7377940bbd8874f6b398f9bd
TarasK1234 <152324765+TarasK1234@users.noreply.github.com>
2024-09-13 14:25:24 -0600

    Assignment 2
    Completed week 2 assignment

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      
+      <p className="text-3xl">CPRG 306: Web Development 2 - Assignments </p>
+      <ul>
+        <li>
+          <Link href="./week-2/" className="underline text-violet-600 hover:text-violet-400">Week 2- Student Info</Link>
+
+        </li>
+      </ul>
     </main>
-   
+
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,13 @@
+import StudentInfo from "./student-info";
+import Link from "next/link";
 
 
 export default function Page() {
     return (
       <main>
-        <h1>Shopping List</h1>
+        <h1>Week 2</h1>
+        <StudentInfo />
+        <Link href="/">Home</Link>
       </main>
     );
   }
